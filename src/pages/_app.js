f618617a4ac70b93dfc46556fdbf69c150301aa6
Rishav Thapliyal <rishabhthapliyal03@gmail.com>
2024-09-17 11:43:46 +0530

    feat:Added analytics component

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,13 @@
 import Layout from "@/components/layout";
 import "../styles/globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   );
 }
