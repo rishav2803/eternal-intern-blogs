fc8fb98012d6f311ed5983f62b95334ca3b34245
Rishav Thapliyal <rishabhthapliyal03@gmail.com>
2024-09-17 21:04:23 +0530

    made some changes

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,6 +10,7 @@ export default function Document() {
         <meta name='author' content='Rishav thapliyal' />
         <link rel='icon' href='/logo.png' />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="google-site-verification" content="gkZahdsaYiK41oDLh9jVaLm4T60Vq7gPzAZ9_lReS6s" />
       </Head>
       <body className="antialiased">
         <Main />
